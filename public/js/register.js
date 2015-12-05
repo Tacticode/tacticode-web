85dbac80e65fddfe0089439b0cbee7dbcebc8404
Arnaud Drain <drain.arnaud@hotmail.fr>
2015-12-05 15:58:18 +0800

    Update registration

--- public/js/register.js
+++ public/js/register.js
@@ -30,23 +30,61 @@ function updateField(field) {
 	var selector = '#' + field.name + '-group';
 
 	$(selector).removeClass('has-success has-error');
-	$('#' + field.name + '-icon').removeClass('fa-check fa-close');
+	$('#' + field.name + '-icon').removeClass('fa-check fa-close fa-spin fa-spinner');
 	$('#' + field.name + '-error').text('');
 
 	if (field.status == 'valid') {
 
 		$(selector).addClass('has-success');
 		$('#' + field.name + '-icon').addClass('fa-check');
-	}
-	else if (field.status == 'error') {
+	} else if (field.status == 'error') {
 
 		$(selector).addClass('has-error');
 		$('#' + field.name + '-error').text(field.error);
 		$('#' + field.name + '-icon').addClass('fa-close');
+	} else if (field.status == 'loading') {
+
+		$('#' + field.name + '-icon').addClass('fa-spin fa-load fa-spinner');
 	}
 }
 
+var loginAjax = null;
+var loginXhr = null;
+
+function checkLoginTaken(field, login) {
+
+	field.status = 'loading';
+	updateField(field);
+
+	loginAjax = setTimeout(function() {
+
+		loginAjax = null;
+		loginXhr = $.getJSON('/checkloginexists/' + login, null, function(data) {
+
+			loginXhr = null;
+			if (data.exists == "true") {
+
+				field.status = 'error';
+				field.error = 'The login has already been taken.';
+			} else
+				field.status = 'valid';
+			updateField(field);
+		});
+	}, 1000);
+}
+
 function checkLogin() {
+
+	if (loginXhr) {
+		
+		loginXhr.abort();
+		loginXhr = null;
+	}
+	if (loginAjax) {
+
+		clearTimeout(loginAjax);
+		loginAjax = null;
+	}
 
 	var login = $(this).val();
 	var field = getField('login');
@@ -59,12 +97,49 @@ function checkLogin() {
 
 		field.status = 'error';
 		field.error = 'Login length should be between 3 and 20 characters.';
-	}
+	} else
+		checkLoginTaken(field, login);
 
 	updateField(field);
 }
 
+var emailAjax = null;
+var emailXhr = null;
+
+function checkEmailTaken(field, email) {
+
+	field.status = 'loading';
+	updateField(field);
+
+	loginAjax = setTimeout(function() {
+
+		loginAjax = null;
+		loginXhr = $.getJSON('/checkemailexists/' + email, null, function(data) {
+
+			loginXhr = null;
+			if (data.exists == "true") {
+
+				field.status = 'error';
+				field.error = 'The email has already been taken.';
+			} else
+				field.status = 'valid';
+			updateField(field);
+		});
+	}, 1000);
+}
+
 function checkEmail() {
+
+	if (emailXhr) {
+		
+		emailXhr.abort();
+		emailXhr = null;
+	}
+	if (emailAjax) {
+
+		clearTimeout(emailAjax);
+		emailAjax = null;
+	}
 
 	var email = $(this).val();
 	var field = getField('email');
@@ -77,7 +152,8 @@ function checkEmail() {
 
 		field.status = 'error';
 		field.error = 'Please enter a valid email.';
-	}
+	} else
+		checkEmailTaken(field, email);
 
 	updateField(field);
 }
