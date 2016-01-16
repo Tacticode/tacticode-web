95a8ae63a778c5c5be43c541125e1460dc937356
Gerardufoin <gerardufoin2@hotmail.com>
2016-01-16 11:38:14 +0100

    Add power model; Home redirect user to dashboard when connected

--- app/Http/Models/Group.php
+++ app/Http/Models/Group.php
@@ -6,8 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
+	/**
+     * The database table used by the model.
+     *
+     * @var string
+     */
     protected $table = 'groups';
 
+    /**
+    * A group has many users.
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\HasMany
+    *
+    */
     public function user()
     {
     	return $this->hasMany('App\Http\Models\User');
