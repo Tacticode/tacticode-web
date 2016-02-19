ba645923d78d1d2ce28ddf728a3cc01a919946d8
Gerardufoin <gerardufoin2@hotmail.com>
2016-02-19 17:50:26 +0100

    Rename node_path into paths to respect bdd logic

---
+++ database/migrations/2016_02_14_112219_create_paths.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaths extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paths', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('node_from')->unsigned();
+            $table->integer('node_to')->unsigned();
+
+            $table->foreign('node_from')->references('id')->on('nodes');
+            $table->foreign('node_to')->references('id')->on('nodes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paths');
+    }
+}
