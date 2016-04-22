1a2eb359768cb9679012c1522a2d2e4bdd6a633a
Thibault Duval <gerardufoin2@hotmail.com>
2016-04-22 15:47:33 +0200

    Add solo and team leaderboard

--- resources/views/leaderboard/index.blade.php
+++ resources/views/leaderboard/index.blade.php
@@ -4,34 +4,23 @@
 @section('content')
     <h1 class="page-header">@lang('leaderboard.title')</h1>
 
-    <div class="table-responsive">
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>@lang('leaderboard.position')</th>
-                    <th>@lang('leaderboard.name')</th>
-                    <th>@lang('leaderboard.score')</th>
-                </tr>
-            </thead>
-            <tbody>
-            	@foreach ($elo as $key => $e)
-            	 	@if ($users[$key-1]->id == Auth::user()->id)
-            	 		<tr class="info">
-		                    <td>{{ array_search($key, array_keys($elo)) + 1 }}</td>
-		                    <td>{{ $users[$key-1]->login }}</td>
-		                    <td>{{ $e }}</td>
-		                    <td><a class="btn btn-primary" href="/user">@lang('leaderboard.myProfile')</a></td>
-		                </tr>
-            	 	@else
-		                <tr>
-		                    <td>{{ array_search($key, array_keys($elo)) + 1 }}</td>
-		                    <td>{{ $users[$key-1]->login }}</td>
-		                    <td>{{ $e }}</td>
-		                    <td><a class="btn btn-primary" href="/users/{{ $users[$key-1]->id }}">@lang('navigation.view')</a></td>
-		                </tr>
-	                @endif
-	            @endforeach
-            </tbody>
-        </table>
+    <ul class="nav nav-pills">
+      <li class="active"><a data-toggle="pill" href="#global">Global</a></li>
+      <li><a data-toggle="pill" href="#solo">Solo</a></li>
+      <li><a data-toggle="pill" href="#team">Team</a></li>
+    </ul>
+
+    <div class="tab-content">
+        <div id="global" class="table-responsive tab-pane fade in active">
+            @include('leaderboard.table_content', ['users' => $users, 'elo' => $elo['global']])
+        </div>
+
+        <div id="solo" class="table-responsive tab-pane fade">
+            @include('leaderboard.table_content', ['users' => $users, 'elo' => $elo['solo']])
+        </div>
+
+        <div id="team" class="table-responsive tab-pane fade">
+            @include('leaderboard.table_content', ['users' => $users, 'elo' => $elo['team']])
+        </div>
     </div>
 @endsection
---
+++ resources/views/leaderboard/table_content.blade.php
@@ -0,0 +1,28 @@
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>@lang('leaderboard.position')</th>
+            <th>@lang('leaderboard.name')</th>
+            <th>@lang('leaderboard.score')</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($elo as $key => $e)
+            @if ($users[$key-1]->id == Auth::user()->id)
+            <tr class="info">
+              <td>{{ array_search($key, array_keys($elo)) + 1 }}</td>
+              <td>{{ $users[$key-1]->login }}</td>
+              <td>{{ $e }}</td>
+              <td><a class="btn btn-primary" href="/user">@lang('leaderboard.myProfile')</a></td>
+            </tr>
+            @else
+              <tr>
+                  <td>{{ array_search($key, array_keys($elo)) + 1 }}</td>
+                  <td>{{ $users[$key-1]->login }}</td>
+                  <td>{{ $e }}</td>
+                  <td><a class="btn btn-primary" href="/users/{{ $users[$key-1]->id }}">@lang('navigation.view')</a></td>
+              </tr>
+            @endif
+        @endforeach
+    </tbody>
+</table>
