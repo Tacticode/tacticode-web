5d6106828fb2e3a9ece5e2bea1026a89e9ae99f1
Arnaud Drain <drain.arnaud@hotmail.fr>
2016-04-22 22:08:01 +0800

    Update link with arena viewer

--- resources/views/arena/viewfight.blade.php
+++ resources/views/arena/viewfight.blade.php
@@ -8,16 +8,18 @@
 	<script src="/arena-viewer/script/map.js"></script>
 	<script src="/arena-viewer/script/projectile.js"></script>
 	<script src="/arena-viewer/script/entity.js"></script>
+	<script src="/arena-viewer/script/customTexture.js"></script>
 	<script src="/arena-viewer/script/fight.js"></script>
-	<script src="/arena-viewer/script/viewer.js"></script>
-	<script src="/arena-viewer/script/shadowtext.js"></script>
-@endsection
+	<script src="/arena-viewer/script/cellinformation.js"></script>
+	<script src="/arena-viewer/script/overlay.js"></script>
+	<script src="/arena-viewer/script/config.js"></script>
 
 @section('content')
     <h1 class="page-header">@lang('arena.fight')</h1>
 
-    @if (!$fight['fight_content'] && 0)
+    @if (!$fight['fight_content'])
     	
+    	{{$fight['id']}}
     	<input id="fightId" name="fightId" value="{{$fight['id']}}" type="hidden">
     
 	    <div class="row arena">
@@ -53,19 +55,51 @@
 				@endif
 			</div>
 			<div class="row">
-				Combat en cours...
 			</div>
 	    </div>
 
-	@elseif (0)
+	@else
 
-		Combat \o/
+		<div class="row">
+			<input id="fightId" name="fightId" value="{{$fight['id']}}" type="hidden">
+			<input id="fightContent" name="fightContent" value="{{$fight['fight_content']}}" type="hidden">
+		</div>
 
     @endif
 
     <div id="viewer"></div>
 
     <script>
+
+    var fight = {
+	"map": "sample",
+	"entities": [
+		{"id": 0, "x": 1, "y": 2, "breed": "orc", "team": 0, "health": 1000},
+		{"id": 1, "x": 1, "y": 3, "breed": "orc", "team": 0, "health": 800},
+		{"id": 2, "x": 6, "y": 4, "breed": "elf", "team": 1, "health": 600}
+	],
+	"actions" : [
+		{"type": "move", "entity": 1, "x": 2, "y": 3},
+		{"type": "move", "entity": 1, "x": 2, "y": 4},
+		{"type": "skill", "entity": 0, "skill": "fireball", "x": 6, "y": 4},
+		{"type": "heal", "entity": 0, "health": 51},
+		{"type": "damage", "entity": 2, "health": 51},
+
+		{"type": "move", "entity": 2, "x": 6, "y": 5},
+		{"type": "skill", "entity": 2, "skill": "arrow", "x": 2, "y": 4},
+		{"type": "damage", "entity": 1, "health": 179},
+		{"type": "heal", "entity": 2, "health": 179},
+		
+		{"type": "move", "entity": 2, "x": 6, "y": 6},
+		{"type": "skill", "entity": 2, "skill": "arrow", "x": 5, "y": 0},
+		{"type": "move", "entity": 2, "x": 5, "y": 6},
+		{"type": "skill", "entity": 2, "skill": "arrow", "x": 25, "y": 0},
+		{"type": "move", "entity": 2, "x": 5, "y": 5},
+		{"type": "skill", "entity": 2, "skill": "arrow", "x": 4, "y": 12},
+		{"type": "move", "entity": 2, "x": 6, "y": 5},
+		{"type": "skill", "entity": 2, "skill": "arrow", "x": -5, "y": 5}
+	]
+};
 
     	var fightId = null;
     	var timeout = null;
@@ -77,13 +111,21 @@
     			console.log(data);
 	    		if (!data.content)
 		    		setTimeout(askFightContent, 1000);
+		    	else {
+
+		    		console.log(data);
+				    Tacticode.init("viewer");
+					Tacticode.loadFight(fight);
+		    	}
     		});
     	}
 
     	$(document).ready(function() {
 
     		fightId = $('#fightId').val();
+    		fightContent = $('#fightContent').val();
     		$('#fightId').remove();
+    		console.log(fightContent);
     		
     		if (fightId)
     			askFightContent();
