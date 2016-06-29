0b7badb5cd44d0734114d2ccd4abe626ea31b13d
Thibault Duval <gerardufoin2@hotmail.com>
2016-06-29 18:55:07 +0200

    Add script null for character and notification to enemy when fighting

---
+++ resources/lang/en/notifications.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Notifications Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in the notifications.
+    |
+    */
+
+    'welcome_title' => 'Welcome !',
+    'welcome_content' => 'Welcome on the site1',
+    'fight_solo_title' => 'New Solo Fight !',
+    'fight_solo_content' => 'Your character \':character\' has been attacked by \':enemy\'.',
+    'fight_team_title' => 'New Team Fight !',
+    'fight_team_content' => 'Your team \':team\' has been attacked by \':enemy\'.',
+
+];
