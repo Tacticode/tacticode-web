3509441b4ed3e271739ea95e84d296a60f920244
Arnaud Drain <drain.arnaud@hotmail.fr>
2016-06-29 19:10:01 +0200

    Remove sample data

--- resources/views/arena/viewfight.blade.php
+++ resources/views/arena/viewfight.blade.php
@@ -71,36 +71,6 @@
 
     <script>
 
-	    var fight = {
-			"map": "sample",
-			"entities": [
-				{"id": 0, "x": 1, "y": 2, "breed": "orc", "team": 0, "health": 1000},
-				{"id": 1, "x": 1, "y": 3, "breed": "orc", "team": 0, "health": 800},
-				{"id": 2, "x": 6, "y": 4, "breed": "elf", "team": 1, "health": 600}
-			],
-			"actions" : [
-				{"type": "move", "entity": 1, "x": 2, "y": 3},
-				{"type": "move", "entity": 1, "x": 2, "y": 4},
-				{"type": "skill", "entity": 0, "skill": "fireball", "x": 6, "y": 4},
-				{"type": "heal", "entity": 0, "health": 51},
-				{"type": "damage", "entity": 2, "health": 51},
-
-				{"type": "move", "entity": 2, "x": 6, "y": 5},
-				{"type": "skill", "entity": 2, "skill": "arrow", "x": 2, "y": 4},
-				{"type": "damage", "entity": 1, "health": 179},
-				{"type": "heal", "entity": 2, "health": 179},
-				
-				{"type": "move", "entity": 2, "x": 6, "y": 6},
-				{"type": "skill", "entity": 2, "skill": "arrow", "x": 5, "y": 0},
-				{"type": "move", "entity": 2, "x": 5, "y": 6},
-				{"type": "skill", "entity": 2, "skill": "arrow", "x": 25, "y": 0},
-				{"type": "move", "entity": 2, "x": 5, "y": 5},
-				{"type": "skill", "entity": 2, "skill": "arrow", "x": 4, "y": 12},
-				{"type": "move", "entity": 2, "x": 6, "y": 5},
-				{"type": "skill", "entity": 2, "skill": "arrow", "x": -5, "y": 5}
-			]
-		};
-
     	var fightId = null;
 
     	function checkSample(fight) {
